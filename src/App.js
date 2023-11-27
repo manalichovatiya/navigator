9ae1c07896740049c0fd8167129de1df0713fd81
MANALI CHOVATIYA <manalichovatiya07@gmail.com>
2023-11-27 07:39:04 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './component/About';
+import Home from './component/Home';
+import Navbar from './component/Navbar';
+import Product from './component/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/about" exact element={<About />} />
+      <Route path="/product" exact element={<Product />} />
+    </Routes>
+  </div>
   );
 }
 
